6f433fa27b18f2cb2137a505de63d3d6b89ef97b
Gabriel Bonifacio Possomato <gabrielb.possomato@gmail.com>
2023-04-14 09:30:34 -0300

    add ex001

---
+++ functions/ex001.js
@@ -0,0 +1,4 @@
+/*
+Escreva um script que pergunta ao usuário se ele deseja converter uma temperatura de grau Celsius para Farenheit ou vice-versa. Para cada opção, crie uma função. Crie uma terceira, que é um menu para o usuário escolher a opção desejada, onde esse menu chama a função de conversão correta.
+Conversão Celsius e Fahrenheit
+*/
